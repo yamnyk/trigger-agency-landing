b945e02b10f8e5f87f231c70a9d63e05f27c2377
Vradiy <denvradiy@gmail.com>
2019-04-15 13:46:56 +0300

    @denvradiy | add form validation, phone mask

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,38 @@
 const gogi = 'gogi';
 const notGogi = 'not gogi';
+
+$( document ).ready(function() {
+
+    $('#phone').mask('+38(999) 999-99-99')
+
+    $('#orderForm').validate({
+        rules: {
+            name: {
+                required: true,
+            },
+            email: {
+                required: true,
+            },
+            phone: {
+                required: true
+            }
+        },
+        messages: {
+            name: {
+                required:'Name field is required.'
+            },
+            email: {
+                required:'Email field is required'
+            },
+            phone: {
+                required:'Phone number field is required.'
+            }
+        }
+    });
+
+
+
+});
+
+
+
