a0eaf0ee75165749157ec72237332524e8ed77a1
denvradiy <49561056+denvradiy@users.noreply.github.com>
2019-04-17 13:24:49 +0300

    add order section (#6)
    * @denvradiy | add template
    
    * @denvradiy | add styles
    
    * @denvradiy | add form validation, phone mask
    
    * @denvradiy | change form style, add sending form (jq part)
    
    * @denvradiy | add php file
    
    * @denvradiy | change slogan styles
    
    * @denvradiy | finish order-button styles, remove redundant height, change class name, change order__info__text paddings on margins
    
    * @denvradiy | change width of slogan on 3vw, add paddings on headline modifier

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,67 @@
 const gogi = 'gogi';
 const notGogi = 'not gogi';
+
+$( document ).ready(function() {
+
+    $('#form__phone').mask('+38(999) 999-99-99')
+
+    $('#orderForm').validate({
+        rules: {
+            name: {
+                required: true,
+            },
+            email: {
+                required: true,
+            },
+            phone: {
+                required: true
+            }
+        },
+        messages: {
+            name: {
+                required:'Name field is required.'
+            },
+            email: {
+                required:'Email field is required'
+            },
+            phone: {
+                required:'Phone number field is required.'
+            }
+        }
+    });
+
+    $(function() {
+        $("#orderForm").submit(function(event) {
+            event.preventDefault();
+            let data = {
+                message:`
+                Заказ индивидуальной консультации:
+                Имя - ${$('#form__name').val()},
+                Email - ${$('#form__email').val()},
+                Телефон - ${$('#form__phone').val()},
+                `,
+
+            };
+            $.ajax({
+                type: "POST",
+                url: "email.php",
+                data: data,
+                success: function(){
+                    alert(`
+                Индивидуальная консультация на
+                Имя - ${$('#form__name').val()},
+                Email - ${$('#form__email').val()},
+                Телефон - ${$('#form__phone').val()},
+                Была успешно заказана `,);
+                }
+            });
+
+            return false;
+        });
+    });
+
+
+});
+
+
+
