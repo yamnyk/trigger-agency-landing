a6a71ac89128e79a8bf599adffcd9567ac4b4a13
andrey <yurist.feo@gmail.com>
2019-04-17 18:51:43 +0300

    @zaitsev firstCommit

--- gulpfile.js
+++ gulpfile.js
@@ -1,105 +1,105 @@
 let gulp = require('gulp'),
-	babel = require('gulp-babel'),
-	prefixer = require('gulp-autoprefixer'),
-	uglify = require('gulp-uglify'),
-	clean = require('gulp-clean'),
-	sass = require('gulp-sass'),
-	concat = require('gulp-concat'),
-	imagemin = require('gulp-imagemin'),
-	browserSync = require("browser-sync").create();
+    babel = require('gulp-babel'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
+    browserSync = require("browser-sync").create();
 
 const path = {
-	build: {
-		html: 'build',
-		js: 'build/js/',
-		css: 'build/css/',
-		img: 'build/img/',
-		fonts: 'build/fonts/'
-	},
-	src: {
-		html: 'src/index.html',
-		js: 'src/js/script.js',
-		style: 'src/scss/**/*.scss',
-		img: 'src/img/**/*.*',
-		fonts: 'src/fonts/**/*.*'
-	},
-	watch: {
-		html: 'src/index.html',
-		js: 'src/js/script.js',
-		style: 'src/scss/**/*.scss',
-		img: 'src/img/**/*.*',
-		fonts: 'src/fonts/**/*.*'
-	},
-	clean: 'build/'
+    build: {
+        html: 'build',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: {
+        html: 'src/index.html',
+        js: 'src/js/script.js',
+        style: 'src/scss/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: {
+        html: 'src/index.html',
+        js: 'src/js/script.js',
+        style: 'src/scss/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './build/'
 };
 
 
 const htmlBuild = () => (
-	gulp.src(path.src.html)
-		.pipe(gulp.dest(path.build.html))
-		.pipe(browserSync.stream())
+    gulp.src(path.src.html)
+        .pipe(gulp.dest(path.build.html))
+        .pipe(browserSync.stream())
 );
 
 const scssBuild = () => (
-	gulp.src(path.src.style)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(concat('style.css'))
-		.pipe(clean())
-		.pipe(prefixer({
-			browsers: ['last 100 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest(path.build.css))
-		.pipe(browserSync.stream())
+    gulp.src(path.src.style)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(clean())
+        .pipe(prefixer({
+            browsers: ['last 100 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(path.build.css))
+        .pipe(browserSync.stream())
 
 );
 
 const jsBuild = () => (
-	gulp.src(path.src.js)
-		.pipe(babel({
-			presets: ['@babel/env']
-		}))
-		.pipe(uglify())
-		.pipe(gulp.dest(path.build.js))
-		.pipe(browserSync.stream())
+    gulp.src(path.src.js)
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(browserSync.stream())
 );
 
 const imgBuild = () => (
-	gulp.src(path.src.img)
-		.pipe(imagemin({
-			progressive: true,
-			svgoPlugins: [{
-				removeViewBox: false
-			}],
-			// use: [pngquant()],
-			interlaced: true
-		}))
-		.pipe(gulp.dest(path.build.img))
-		.pipe(browserSync.stream())
+    gulp.src(path.src.img)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{
+                removeViewBox: false
+            }],
+            // use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(browserSync.stream())
 );
 
 const fontsBuild = () => (
-	gulp.src(path.src.fonts)
-		.pipe(gulp.dest(path.build.fonts))
-		.pipe(browserSync.stream())
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+        .pipe(browserSync.stream())
 );
 
 const watcher = () => {
-	browserSync.init({
-		server: {
-			baseDir: "./build"
-		}
-	});
-	gulp.watch(path.watch.html, htmlBuild).on('change', browserSync.reload);
-	gulp.watch(path.watch.style, scssBuild).on('change', browserSync.reload);
-	gulp.watch(path.watch.js, jsBuild).on('change', browserSync.reload);
-	gulp.watch(path.watch.img, imgBuild).on('change', browserSync.reload);
-	gulp.watch(path.watch.fonts, fontsBuild).on('change', browserSync.reload);
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+    gulp.watch(path.watch.html, htmlBuild).on('change', browserSync.reload);
+    gulp.watch(path.watch.style, scssBuild).on('change', browserSync.reload);
+    gulp.watch(path.watch.js, jsBuild).on('change', browserSync.reload);
+    gulp.watch(path.watch.img, imgBuild).on('change', browserSync.reload);
+    gulp.watch(path.watch.fonts, fontsBuild).on('change', browserSync.reload);
 };
 
 const cleanBuild = () => (
-	gulp.src(path.clean, {read: false})
-		.pipe(clean())
+    gulp.src(path.clean, {allowEmpty: true})
+        .pipe(clean())
 );
 
 
@@ -114,10 +114,10 @@ gulp.task('watcher', watcher);
 gulp.task('clean', cleanBuild);
 
 gulp.task('default', gulp.series(
-	cleanBuild,
-	htmlBuild,
-	scssBuild,
-	jsBuild,
-	gulp.parallel(fontsBuild,imgBuild),
-	watcher
+    cleanBuild,
+    htmlBuild,
+    scssBuild,
+    jsBuild,
+    gulp.parallel(fontsBuild,imgBuild),
+    watcher
 ));
